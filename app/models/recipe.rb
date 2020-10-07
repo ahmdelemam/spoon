39ea3f7df5a3f8e441973dba3bee3b121622e51f
ahmdelemam <ahmdelemam@gmail.com>
2020-10-07 22:16:32 +0200

    add recipe model validation using shoulda matchers

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
+  validates :title, :photo, :description, presence: true
+
+  belongs_to :chef
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:chef) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:photo) }
+    it { should validate_presence_of(:description) }
+  end
 end
